d35bf91737447db79dec1ec84e590f9132a6d7cd
namjsdev <nam.js.dev@gmail.com>
2022-11-08 22:05:20 +0700

    new

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
-var settings = {
+var mql = window.matchMedia('screen and (min-width: 768px)');
+    if (mql.matches){
+        // window width is at least 768px
+        var settings = {
 
-    particles: {
+            particles: {
+        
+                length: 700, // maximum amount of particles
+        
+                duration: 2, // particle duration in sec
+        
+                velocity: 100, // particle velocity in pixels/sec
+        
+                effect: -1, // play with this for a nice effect
+        
+                size: 25, // particle size in pixels
+        
+            },
+        
+        };
+    } else {
+        // window width is less than 768px
+        var settings = {
 
-        length: 500, // maximum amount of particles
-
-        duration: 2, // particle duration in sec
-
-        velocity: 100, // particle velocity in pixels/sec
-
-        effect: -0.75, // play with this for a nice effect
-
-        size: 30, // particle size in pixels
-
-    },
-
-};
+            particles: {
+        
+                length: 400, // maximum amount of particles
+        
+                duration: 2, // particle duration in sec
+        
+                velocity: 25, // particle velocity in pixels/sec
+        
+                effect: -2, // play with this for a nice effect
+        
+                size: 10, // particle size in pixels
+        
+            },
+        
+        };
+    }
 
 (function () { var b = 0; var c = ["ms", "moz", "webkit", "o"]; for (var a = 0; a < c.length && !window.requestAnimationFrame; ++a) { window.requestAnimationFrame = window[c[a] + "RequestAnimationFrame"]; window.cancelAnimationFrame = window[c[a] + "CancelAnimationFrame"] || window[c[a] + "CancelRequestAnimationFrame"] } if (!window.requestAnimationFrame) { window.requestAnimationFrame = function (h, e) { var d = new Date().getTime(); var f = Math.max(0, 16 - (d - b)); var g = window.setTimeout(function () { h(d + f) }, f); b = d + f; return g } } if (!window.cancelAnimationFrame) { window.cancelAnimationFrame = function (d) { clearTimeout(d) } } }());
 
